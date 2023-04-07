47a1170ff9410bf484602b4d3328e2e4166d596f
ucctn <chetan.singhal@ucertify.com>
2023-04-07 16:13:12 -0700

    This is a test commit.

---
+++ courseProjectPhase1/src/courseProjectPhase1/CIS406.java
@@ -0,0 +1,13 @@
+package courseProjectPhase1;
+
+public class CIS406 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.print("Congratulations on your first Java program");
+
+	}
+
+}
